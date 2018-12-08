1f671422e5f8f923f4dbe5062053ade6f9328589
Geisiane <geisiane.aa@gmail.com>
2018-12-08 19:59:19 -0200

    Pré-processamento dos dados (transformação, escalonamento e divisão da base em treinamento e teste)

---
+++ pre_processamento_income-census-database.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  8 18:34:17 2018
+
+@author: GeisyPC
+"""
+
+import pandas as pd
+
+base = pd.read_csv('census.csv')
+
+previsores = base.iloc[:, 0:14].values
+classe = base.iloc[:, 14].values
+
+# importar a classe/package 
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_previsores = LabelEncoder()
+# tranformação de variáveis categóricas em variáveis numéricas, para a analise do algoritmo de aprendizagem de máquina.
+previsores[:, 1] = labelencoder_previsores.fit_transform(previsores[:, 1])
+previsores[:, 3] = labelencoder_previsores.fit_transform(previsores[:, 3])
+previsores[:, 5] = labelencoder_previsores.fit_transform(previsores[:, 5])
+previsores[:, 6] = labelencoder_previsores.fit_transform(previsores[:, 6])
+previsores[:, 7] = labelencoder_previsores.fit_transform(previsores[:, 7])
+previsores[:, 8] = labelencoder_previsores.fit_transform(previsores[:, 8])
+previsores[:, 9] = labelencoder_previsores.fit_transform(previsores[:, 9])
+previsores[:, 13] = labelencoder_previsores.fit_transform(previsores[:, 13])
+
+# Ajustes em variáveis categóricas nominais (ajustes dos pesos para as variáveis do tipo Dummy) 
+onehotencoder = OneHotEncoder(categorical_features = [1,3,5,6,7,8,9,13])
+# transforma todos os atributos
+previsores = onehotencoder.fit_transform(previsores).toarray()
+
+# Encoder para a transformação da variável classe para numérico
+labelencoder_classe = LabelEncoder()
+classe = labelencoder_classe.fit_transform(classe)
+
+# escalonamento dos dados do previsores
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+# Criando divisão da base de dados de treinamento e a base de teste
+from sklearn.cross_validation import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.15, random_state=0)
+
+# importação da biblioteca
+# criação do classificador
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
