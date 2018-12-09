aa6bd5dcbdd348fbd2f091b69adb60fd4c08526f
Geisiane <geisiane.aa@gmail.com>
2018-12-08 23:37:20 -0200

    Aplicação do algoritmo naive bayes utilizando scikit-learn

---
+++ naive-bayes-census-database.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  8 22:56:28 2018
+
+@author: GeisyPC
+"""
+
+import pandas as pd
+
+base = pd.read_csv('census.csv')
+
+previsores = base.iloc[:, 0:14].values
+classe = base.iloc[:, 14].values
+                
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_previsores = LabelEncoder()
+previsores[:, 1] = labelencoder_previsores.fit_transform(previsores[:, 1])
+previsores[:, 3] = labelencoder_previsores.fit_transform(previsores[:, 3])
+previsores[:, 5] = labelencoder_previsores.fit_transform(previsores[:, 5])
+previsores[:, 6] = labelencoder_previsores.fit_transform(previsores[:, 6])
+previsores[:, 7] = labelencoder_previsores.fit_transform(previsores[:, 7])
+previsores[:, 8] = labelencoder_previsores.fit_transform(previsores[:, 8])
+previsores[:, 9] = labelencoder_previsores.fit_transform(previsores[:, 9])
+previsores[:, 13] = labelencoder_previsores.fit_transform(previsores[:, 13])
+
+labelencoder_classe = LabelEncoder()
+classe = labelencoder_classe.fit_transform(classe)
+
+from sklearn.model_selection  import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.15, random_state=0)
+
+# constroi a tabela de probabilidades
+from sklearn.naive_bayes import GaussianNB
+classificador = GaussianNB()
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+# Visualização do percentual/quantidade de erros do registos de previsãos 
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
